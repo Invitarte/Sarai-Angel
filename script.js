f96487b4926562ee71dad344422ea1e40a660505
Invitarte <saraicarrillo_08@outlook.com>
2026-01-11 12:12:40 -0600

    Implement image carousel with navigation functions
    Added image carousel functionality with next and previous controls.

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ setInterval(() => {
   document.getElementById("minutes").innerText = minutes;
   document.getElementById("seconds").innerText = seconds;
 }, 1000);
+
+const images = [
+  "img/foto1.jpg",
+  "img/foto2.jpg",
+  "img/foto3.jpg"
+];
+
+let currentIndex = 0;
+const carouselImage = document.getElementById("carouselImage");
+
+function showImage(index) {
+  carouselImage.src = images[index];
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
